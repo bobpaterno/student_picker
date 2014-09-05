364ff9599c9ca6eef53a0ac9db0ee025fd4cd722
Robert Paterno <bobpaterno@gmail.com>
2014-09-05 10:00:37 -0500

    To deploy to heroku

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ActiveRecord::Base
   validates_presence_of :name
 
+  default_scope lambda() { order("last_called ASC NULLS FIRST") }
   scope :pickable, ->{
     where("last_called is null OR last_called < ?", 5.minutes.ago)
   }
